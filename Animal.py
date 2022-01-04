bb49b5eca931913d32a47f531f9225ca901a3cd2
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-01-04 14:59:00 -0600

    Program works

---
+++ Animal.py
@@ -0,0 +1,7 @@
+# Author: Lilliana Parra
+# GitHub Username: ParraL1
+# Date: 1/4/2021
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
